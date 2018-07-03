a6164a7b0fc7d8fc31addc5e6d886b982ac636aa
Paulo Tenorio <40776522+paulotrakto@users.noreply.github.com>
2018-07-03 09:08:10 -0300

    Create conversordeidadedecachorro.js

---
+++ conversordeidadedecachorro.js
@@ -0,0 +1,12 @@
+// colocando uma constante com minha idade
+const myAge=37;
+let earlyYears=2;
+earlyYears *=10.5;
+//aqui eu tiro a diferença dos dois primeiros anos de vida do resto do cálculo
+let laterYears=myAge-2;
+laterYears*=4;
+console.log(laterYears);
+console.log(earlyYears);
+myAgeInDogYears=laterYears+earlyYears;
+let myName= "Paulo Tenorio".toLowerCase();
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} in dog years.`);
